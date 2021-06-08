604b03565a7ecfab6888c40518db4632dc723614
adityaholkar <aditya.holkar17@vit.edu>
2021-06-08 06:13:50 -0700

    Created JAVA project and perform basic string operation

---
+++ Tutorial3/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        int myInt=10;
+        String text = "Hello";
+        String name = "Bob";
+        String blank = " ";
+        double myDouble = 10.98;
+        String greeting = text + blank +name;
+        System.out.println(greeting);
+        System.out.println("Hello" + " " + name);
+        System.out.println("My integer is: "+ myInt);
+        System.out.println("my number is: "+ myDouble);
+    }
+}
