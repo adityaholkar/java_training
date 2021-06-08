a78d8d9af7b2ee742dfee2796725e7ce619b6d01
adityaholkar <aditya.holkar17@vit.edu>
2021-06-08 05:00:24 -0700

    used 8 basic datatype

--- Application.java
+++ Application.java
@@ -1,5 +1,21 @@
 public class Application{
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int myNumber = 237;
+        short myShort = 75;//16 bit
+        long myLong = 382782;
+        double myDouble = 38.23;
+        float myFloat = 88.2f;//for float f is must else it will be double default
+        char myChar = 'Y';
+        boolean myBoolean = false;
+        byte myByte = 123;//-127 to 127
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
     }
 }
