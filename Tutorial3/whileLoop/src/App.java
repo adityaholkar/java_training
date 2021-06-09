2433a223a9ddf2897517a9f84c3faa0978cea134
adityaholkar <aditya.holkar17@vit.edu>
2021-06-09 01:02:27 -0700

    completed while loop tutorial

---
+++ Tutorial3/whileLoop/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int value = 10;
+        while(value<20){
+            System.out.println ("Hello World!! " + value) ;
+            value = value + 1 ;
+        }
+    }
+}
