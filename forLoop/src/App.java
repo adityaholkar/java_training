647c5c41fef015b531adb267ae940c21829ba07a
adityaholkar <aditya.holkar17@vit.edu>
2021-06-09 01:46:09 -0700

    Hands on for loop

---
+++ forLoop/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for(int i=0;i<10;i++){
+            System.out.println("Value of i is : " + i);
+        
+        }
+        for(int i=0;i<5;i++){
+            System.out.printf("value of i is : %d\n",i);
+        }
+    }
+}
