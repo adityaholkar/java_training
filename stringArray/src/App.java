6e9c87cde8ee6ab267175f18a07e9ea75cd6a432
adityaholkar <aditya.holkar17@vit.edu>
2021-06-10 15:39:49 +0530

    example of string array

---
+++ stringArray/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] names = new String[3];
+        names[0] = "Aman";
+        names[1]= "suraj";
+        names[2] = "pawan";
+        for(String name:names ){
+            System.out.println(name);
+        }
+    }
+}
