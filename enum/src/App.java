16cea8f83d5cb2aee919bba5e767f25cacd47db7
adityaholkar <aditya.holkar17@vit.edu>
2021-06-22 15:55:58 +0530

    enum

---
+++ enum/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Number number = Number.ONE;
+        switch(number){
+            case ONE:
+                System.out.println("One");
+                break;
+            case TWO:
+                System.out.println("Two");
+                break;
+            case Three:
+            System.out.println("Three");
+                break;
+            default:
+                break;
+
+        }
+    }
+}
